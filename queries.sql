7d03f2b1f0022783c58238606916d035bbfd001f
PrantoshB <Pbiswas1007@gmail.com>
2023-02-23 13:46:26 +0530

    Write queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,10 @@ SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE name != 'Gabumon';
+
+BEGIN;
+update animals set species = 'unspecified';
+select * from animals;
+rollback;
+select * from animals;
+
